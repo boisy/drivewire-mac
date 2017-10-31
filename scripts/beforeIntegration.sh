2620a942e60b107f17de368037d47ba0ddf376f6
Boisy Pitre <boisy.pitre@tee-boy.com>
2017-10-31 06:56:53 -0500

    Fixing script

--- scripts/beforeIntegration.sh
+++ scripts/beforeIntegration.sh
@@ -3,7 +3,6 @@
 # Called by the Xcode Bot
 export LANG=en_US.UTF-8
 export PATH=/usr/local/opt/ruby/bin:/usr/local/bin:$PATH
-cd ${XCS_SOURCE_DIR}
 echo "Running POD INSTALL"
 #pod update
 pod install

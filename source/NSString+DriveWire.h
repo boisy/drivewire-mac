a2bd829926490e0f330875c503251fe33340fc1f
Boisy Pitre <boisy.pitre@tee-boy.com>
2018-01-04 15:19:40 -0600

    Moved files around

---
+++ source/NSString+DriveWire.h
@@ -0,0 +1,15 @@
+//
+//  NSString+DriveWire.h
+//  DriveWire
+//
+//  Created by Boisy Pitre on 10/18/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (DriveWire)
+
+- (NSString *)stringByProcessingBackspaces;
+- (BOOL)isInteger;
+
+@end

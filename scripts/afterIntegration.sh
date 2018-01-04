0c3fbd496df3a47074b30dde0191349a84e3663d
Boisy Pitre <boisy.pitre@tee-boy.com>
2018-01-04 15:28:25 -0600

    Updating integration scripts

--- scripts/afterIntegration.sh
+++ scripts/afterIntegration.sh
@@ -8,10 +8,10 @@ fi
 marketingVersion="1.0.${XCS_INTEGRATION_NUMBER}"
 pushd "${XCS_ARCHIVE}/Products/Applications"
 zip -yr /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}.zip "DriveWire.app"
-scp /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}.zip administrator@downloads.weathersnoop.com:/Library/Server/Web/Data/Sites/downloads.weathersnoop.com
+scp /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}.zip administrator@downloads.tandycolorcomputer.com:/Library/Server/Web/Data/Sites/downloads.weathersnoop.com
 popd
 zip -yr /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}_dSYMs.zip dSYMs
-scp /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}_dSYMs.zip administrator@downloads.weathersnoop.com:/Library/Server/Web/Data/Sites/downloads.weathersnoop.com
+scp /tmp/DriveWire_${XCS_INTEGRATION_NUMBER}_dSYMs.zip administrator@downloads.tandycolorcomputer.com:/Library/Server/Web/Data/Sites/downloads.tandycolorcomputer.com
 
 # Setup variables to be used in sed
 scp administrator@tandycolorcomputer.com:DriveWire_dsa_priv.pem /tmp
@@ -25,16 +25,16 @@ sed -e "s?__BUNDLEVERSION__?${XCS_INTEGRATION_NUMBER}?" DriveWireAppcast.xml | s
 git config --global user.email "boisy.pitre@tee-boy.com"
 git config --global user.name "Boisy Pitre"
 git log --pretty=format:"<li>%s</li>" `git describe --tags --abbrev=0`..HEAD > /tmp/DriveWire_ChangeLog.html
-scp /tmp/DriveWire_ChangeLog.html administrator@downloads.weathersnoop.com:/Library/Server/Web/Data/Sites/downloads.weathersnoop.com
+scp /tmp/DriveWire_ChangeLog.html administrator@downloads.tandycolorcomputer.com:/Library/Server/Web/Data/Sites/downloads.tandycolorcomputer.com
 
 # tag this latest build
 git tag -a "$marketingVersion-${XCS_INTEGRATION_NUMBER}" -m "$marketingVersion-${XCS_INTEGRATION_NUMBER}"
 git push --follow-tags
 
 # copy appcast
-scp /tmp/DriveWireAppcast.xml administrator@downloads.weathersnoop.com:/Library/Server/Web/Data/Sites/downloads.weathersnoop.com
+scp /tmp/DriveWireAppcast.xml administrator@downloads.tandycolorcomputer.com:/Library/Server/Web/Data/Sites/downloads.tandycolorcomputer.com
 
 # soft link
-ssh -l administrator downloads.weathersnoop.com "cd /Library/Server/Web/Data/Sites/downloads.weathersnoop.com; rm DriveWire.zip; ln -s DriveWire_${XCS_INTEGRATION_NUMBER}.zip DriveWire.zip"
+ssh -l administrator downloads.tandycolorcomputer.com "cd /Library/Server/Web/Data/Sites/downloads.tandycolorcomputer.com; rm DriveWire.zip; ln -s DriveWire_${XCS_INTEGRATION_NUMBER}.zip DriveWire.zip"
 
-ssh -l administrator downloads.weathersnoop.com "cd /Library/Server/Web/Data/Sites/downloads.weathersnoop.com; rm DriveWire_dSYMs.zip; ln -s DriveWire_${XCS_INTEGRATION_NUMBER}_dSYMs.zip DriveWire_dSYMs.zip"
+ssh -l administrator downloads.tandycolorcomputer.com "cd /Library/Server/Web/Data/Sites/downloads.tandycolorcomputer.com; rm DriveWire_dSYMs.zip; ln -s DriveWire_${XCS_INTEGRATION_NUMBER}_dSYMs.zip DriveWire_dSYMs.zip"

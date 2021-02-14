b3ae214fb03220341ac78908696effcd919c28d4
Boisy Pitre <boisy.pitre@tee-boy.com>
2021-02-13 20:41:35 -0600

    Updating Sparkle support

--- scripts/beforeIntegration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Before Integration Run Script
-# Called by the Xcode Bot
-export LANG=en_US.UTF-8
-export PATH=/usr/local/opt/ruby/bin:/usr/local/bin:$PATH
-echo "Running POD INSTALL"
-#pod update
-pod install
-
